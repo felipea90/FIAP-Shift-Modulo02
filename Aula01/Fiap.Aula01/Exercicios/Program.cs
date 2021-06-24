1ab9424e51a7fce9ce121371e792eb689ebfde90
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-24 20:45:34 -0300

    Exercicios 1 e 2 - Aula02

---
+++ Aula01/Fiap.Aula01/Exercicios/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto focados nos Exercícios do Módulo 02");
+
+            //#################################
+
+            //Exercicio 01
+            Console.WriteLine("Cálculo do Custo final de um carro ao consumidor.");
+
+            //Ler valor do veículo
+            Console.WriteLine("Informe o valor de custo da fabricação:");
+            float fabricacao = float.Parse(Console.ReadLine());
+
+            //Realizar os cálculos
+            float distribuidor = (float)(fabricacao * 0.29);
+            float impostos = (float)(fabricacao * 0.46);
+            decimal produtoFinal = (decimal)(fabricacao + distribuidor + impostos);
+
+            //Imprimir valor final na tela
+            Console.WriteLine($"O valor final do carro zero km será de R$: {produtoFinal}");
+
+            //#################################
+
+            //Exercício 02
+            Console.WriteLine("Cálculo de comissão de Vendas.");
+
+            double salario = 1500;
+            double comissao;
+            double venda;
+            
+            Console.WriteLine("Informe o valor final da venda:");
+            venda = double.Parse(Console.ReadLine());
+
+            if (venda <= 1500)
+            {
+                comissao = venda * 0.05;
+                Console.WriteLine($"A comissão sobre a venda foi de R$ {comissao}");
+                comissao = salario + (venda * 0.05);
+                Console.WriteLine($"O Salário final será de R$ {comissao}");
+            }
+            else
+            {
+                comissao = 75 + ((venda -1500) * 0.1);
+                Console.WriteLine($"A comissão sobre a venda foi de R$ {comissao}");
+                comissao = salario + comissao;
+                Console.WriteLine($"O Salário final será de R$ {comissao}");
+            }
+
+            //#################################
+
+
+
+        }
+    }
+}
---
+++ Aula01/Fiap.Aula01/Fiap.Aula01.Exemplo01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fiap.Aula01.Exemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler uma string
+            //cw -> tab tab
+            Console.WriteLine("Informe o seu nome:");
+            string nome = Console.ReadLine();
+
+            //Ler um número
+            Console.WriteLine("Informe a sua idade:");
+            int numero = int.Parse(Console.ReadLine());
+
+            //Exibir um dado
+            Console.WriteLine($" Olá {nome} você têm {numero} anos.");
+            
+        }
+    }
+}
