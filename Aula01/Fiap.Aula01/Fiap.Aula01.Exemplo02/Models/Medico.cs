6ef6a3f4de726b929aecd24be136fdf9e379c7c8
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-24 22:03:55 -0300

    Exemplo 2 - Aula02

---
+++ Aula01/Fiap.Aula01/Fiap.Aula01.Exemplo02/Models/Medico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula01.Exemplo02.Models
+{
+    public class Medico
+    {
+        //Atributos (características)
+
+
+        //Métodos (ações)
+
+    }
+}
