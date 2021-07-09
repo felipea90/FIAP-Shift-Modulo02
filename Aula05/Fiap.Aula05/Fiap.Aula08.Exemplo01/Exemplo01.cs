d518136ca230764c03763ea18c7de6213cfad5d3
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-07-08 21:47:50 -0300

    Aula 08 exemplo 01 - Try Catch Finally

--- Aula05/Fiap.Aula05/Fiap.Aula08.Exemplo01/Exemplo01.cs
+++ Aula05/Fiap.Aula05/Fiap.Aula08.Exemplo01/Exemplo01.cs
@@ -7,6 +7,25 @@ namespace Fiap.Aula08.Exemplo01
     {
         static void Main(string[] args)
         {
+
+            //Tratamento de exception
+            try
+            {
+                //Ler um número
+                Console.WriteLine("Digite um número");
+                var numero = int.Parse(Console.ReadLine());
+            }
+            //Erro -> gera uma mensagem de exceção
+            catch (FormatException)
+            {
+                Console.WriteLine("Caractere inválido");
+            }
+            //Sempre executa o bloco de código -> não é obrigatório
+            finally
+            {
+                Console.WriteLine("Sempre Executa");
+            }
+
             //Ler a mensagem
             Console.WriteLine("Digite a mensagem");
             var msg = Console.ReadLine();
